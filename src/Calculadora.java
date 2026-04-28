912ef3a486d0d0ede0d845b6ab34b6eb05f5813f
raphazpucpr <raphaelzakovicz00@gmail.com>
2026-04-27 21:47:05 -0300

    Create Calculadora.java

---
+++ src/Calculadora.java
@@ -0,0 +1,22 @@
+public class Calculadora {
+
+    public int soma(int a, int b) {
+        return a + b;
+    }
+
+    public int subtracao(int a, int b) {
+        return a - b;
+    }
+
+    public int multiplicacao(int a, int b) {
+        return a * b;
+    }
+
+    public int divisao(int a, int b) {
+        return a / b;
+    }
+
+    public int dobro(int a) {
+        return a * 2;
+    }
+}
